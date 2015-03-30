b36deb42b9d4b7cb792bf500baeef536a3b214a6
Ben <benjamin.chenebault@gmail.com>
2015-03-30 22:05:23 +0200

    feat: implement concurrent version with waitgroup

--- src/github.com/myapp/main.go
+++ src/github.com/myapp/main.go
@@ -6,6 +6,7 @@ import (
 	"io/ioutil"
 	"os"
 	"strings"
+	"sync"
 	"time"
 )
 
@@ -13,11 +14,6 @@ const (
 	DUMP_DIR = "/home/bchenebault/DEV/github/wordCounter/wikipedia-articles/"
 	WORD     = "Theory"
 )
-
-type WordCount struct {
-	word     string
-	numOfOcc int
-}
 
 /*
   Count words occurences in wikipedia dump
@@ -29,9 +25,22 @@ func main() {
 	files, _ := ioutil.ReadDir(DUMP_DIR)
 	index := make(map[string]int)
 
+	wordsChannel := make(chan string)
+	wg := sync.WaitGroup{}
+	wg.Add(len(files))
+
+	// Start counter
+	go func(wc chan string) {
+		for {
+			index[<-wc]++
+		}
+	}(wordsChannel)
+
+	// Start readers
 	for _, f := range files {
-		countWords(f, index)
+		go countWords(f, wordsChannel, wg)
 	}
+
 	fmt.Println("All wikipedia articles indexed in : ", time.Since(startTime))
 
 	reader := bufio.NewReader(os.Stdin)
@@ -47,13 +56,14 @@ func main() {
 	}
 }
 
-func countWords(f os.FileInfo, words map[string]int) {
+func countWords(f os.FileInfo, wc chan string, wg sync.WaitGroup) {
 	fmt.Println(DUMP_DIR + f.Name())
 	file, _ := os.Open(DUMP_DIR + f.Name())
 	lines := bufio.NewScanner(file)
 	for lines.Scan() {
 		for _, w := range strings.Fields(lines.Text()) {
-			words[w]++
+			wc <- w
 		}
 	}
+	wg.Done()
 }
