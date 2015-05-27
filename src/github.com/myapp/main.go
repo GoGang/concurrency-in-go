7fe54d540930af9146d54c2f68a20c47927f6b24
Ben <benjamin.chenebault@gmail.com>
2015-05-27 23:11:42 +0200

    // version

--- src/github.com/myapp/main.go
+++ src/github.com/myapp/main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io/ioutil"
 	"os"
+	"runtime"
 	"strings"
 	"time"
 )
@@ -15,15 +16,39 @@ const DUMP_DIR = "/home/bchenebault/wikipedia-articles/"
   Count words occurences in wikipedia dump
 */
 func main() {
-	fmt.Println("Start indexing wikipedia dump")
+	runtime.GOMAXPROCS(1)
 
-	startTime := time.Now()
+	// Load wikipedia in memory
+	fmt.Println("Loading content in memory")
+	inmemory := make(map[string][]string)
 	files, _ := ioutil.ReadDir(DUMP_DIR)
-	index := make(map[string]int)
-
 	for _, f := range files {
-		countWords(f, index)
+		file, _ := os.Open(DUMP_DIR + f.Name())
+		lines := bufio.NewScanner(file)
+		for lines.Scan() {
+			inmemory[f.Name()] = append(inmemory[f.Name()], lines.Text())
+		}
 	}
+
+	// Start MAP/REDUCE
+	fmt.Println("Start indexing wikipedia dump")
+	startTime := time.Now()
+	index := make(map[string]int)
+	channel := make(chan map[string]int, 100)
+
+	for name, content := range inmemory {
+		fmt.Println("Start worker for file", name)
+		go countWords(content, channel)
+	}
+
+	for i := 0; i < len(files); i++ {
+		partialIndex := <-channel
+		fmt.Println("get one partial index to merge, num of entries :", len(partialIndex))
+		for key, value := range partialIndex {
+			index[key] = index[key] + value
+		}
+	}
+
 	fmt.Println("All wikipedia articles indexed in : ", time.Since(startTime))
 
 	reader := bufio.NewReader(os.Stdin)
@@ -39,13 +64,12 @@ func main() {
 	}
 }
 
-func countWords(f os.FileInfo, words map[string]int) {
-	fmt.Println(DUMP_DIR + f.Name())
-	file, _ := os.Open(DUMP_DIR + f.Name())
-	lines := bufio.NewScanner(file)
-	for lines.Scan() {
-		for _, w := range strings.Fields(lines.Text()) {
+func countWords(content []string, channel chan map[string]int) {
+	words := make(map[string]int)
+	for _, title := range content {
+		for _, w := range strings.Fields(title) {
 			words[w]++
 		}
 	}
+	channel <- words
 }
